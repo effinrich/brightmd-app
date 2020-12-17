5f6481b5baa2706c8cf883b9f78788d59fdcf7e2
Rich <rich@freebirdrides.com>
2020-12-17 15:30:17 -0800

    CRA and storybook config

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  /*
+    This file is unused for configuration, and is only here to satisfy the
+    VSCode Prettier plugin's "requireConfig" setting
+  */
+  tabWidth: 2,
+  singleQuote: true,
+  semi: false
+}
