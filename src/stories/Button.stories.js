ef1cb8a71a7f8f8333a04e07c408b9252f937c32
Rich <rich@freebirdrides.com>
2020-12-17 21:32:55 -0800

    update

--- src/stories/Button.stories.js
+++ src/stories/Button.stories.js
@@ -15,22 +15,22 @@ const Template = (args) => <Button {...args} />
 export const Primary = Template.bind({})
 Primary.args = {
   primary: true,
-  label: 'Button',
+  label: 'Primary Button',
 }
 
 export const Secondary = Template.bind({})
 Secondary.args = {
-  label: 'Button',
+  label: 'Secondary Button',
 }
 
 export const Large = Template.bind({})
 Large.args = {
   size: 'large',
-  label: 'Button',
+  label: 'Large Button',
 }
 
 export const Small = Template.bind({})
 Small.args = {
   size: 'small',
-  label: 'Button',
+  label: 'Small Button',
 }
