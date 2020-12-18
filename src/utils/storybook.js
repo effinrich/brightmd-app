c5ce2f763cc03d4c20c918744388b13718877249
Rich <rich@freebirdrides.com>
2020-12-17 17:12:22 -0800

    storybook util

---
+++ src/utils/storybook.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import { ThemeProvider, createGlobalStyle } from 'styled-components'
+
+import theme from 'theme'
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    font-family: 'Roboto', sans-serif;
+  }
+  body {
+    background-color: ${theme.bodyBackgroundColor};
+    color: ${theme.defaultTextColor};
+  }
+  
+  a {
+    color: ${theme.brandColor};
+    text-decoration: none;
+
+    &:hover {
+      text-decoration: underline;
+    }
+  }
+
+  input:-webkit-autofill,
+  input:-webkit-autofill:hover,
+  input:-webkit-autofill:focus textarea:-webkit-autofill,
+  textarea:-webkit-autofill:hover textarea:-webkit-autofill:focus,
+  select:-webkit-autofill,
+  select:-webkit-autofill:hover,
+  select:-webkit-autofill:focus {
+    -webkit-box-shadow: 0 0 0px 1000px #ffffff inset !important;
+  }
+`
+
+export default Decorator => (
+  <ThemeProvider theme={theme}>
+    {Decorator()}
+    <GlobalStyle />
+  </ThemeProvider>
+)
