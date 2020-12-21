847a88a6dd43d98879c7a3f745e310f3c82bdc46
Rich <rich@freebirdrides.com>
2020-12-21 12:58:26 -0800

    wrap app in provider and theme

--- src/index.js
+++ src/index.js
@@ -2,18 +2,29 @@
 
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { ThemeProvider } from 'emotion-theming'
+import loadable from '@loadable/component'
 import LogRocket from 'logrocket'
 
-import './index.css'
-import App from 'views/App'
+import theme from 'theme'
+import store from 'store'
+
 import reportWebVitals from './reportWebVitals'
+
+// @loadable/component provides the same benefit of React's Lazy/Suspense code-splitting components, but doesn't require the Suspense component, so is a bit more efficient.
+const App = loadable(/* istanbul ignore next */ () => import('views/App'))
 
 // app monitoring service
 LogRocket.init('8jgrsv/brightmd')
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 )
